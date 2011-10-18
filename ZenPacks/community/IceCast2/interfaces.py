c10c3ca660a3e199c2cafb13edbb6693f34ddc80
Érico Andrei <erico@simplesconsultoria.com.br>
2011-10-17 23:44:13 -0200

    Fix Interface

--- ZenPacks/community/IceCast2/interfaces.py
+++ ZenPacks/community/IceCast2/interfaces.py
@@ -23,6 +23,22 @@ class IIceCastDataSourceInfo(IRRDDataSourceInfo):
                                     group=_t('IceCast Monitor'))
 
 
-class IIceCastMountDataSourceInfo(IIceCastDataSourceInfo):
+class IIceCastMountDataSourceInfo(IRRDDataSourceInfo):
+    timeout = schema.Int(title=_t(u'Timeout (seconds)'))
+    cycletime = schema.Int(title=_t(u'Cycle Time (seconds)'))
+    hostname = schema.Text(title=_t(u'Host Name'),
+                           group=_t('IceCast Monitor'))
+    port = schema.Int(title=_t(u'Port'),
+                      group=_t('IceCast Monitor'))
+    ipAddress = schema.Text(title=_t(u'Ip Address'),
+                            group=_t('IceCast Monitor'))
+    url = schema.Text(title=_t(u'URL'),
+                      group=_t('IceCast Monitor'))
     mount = schema.Text(title=_t(u'Mount'),
                       group=_t('IceCast Monitor'))
+    useSsl = schema.Bool(title=_t(u'Use SSL?'),
+                         group=_t('IceCast Monitor'))
+    basicAuthUser = schema.Text(title=_t(u'Basic Auth User'),
+                                group=_t('IceCast Monitor'))
+    basicAuthPass = schema.Password(title=_t(u'Basic Auth Password'),
+                                    group=_t('IceCast Monitor'))
